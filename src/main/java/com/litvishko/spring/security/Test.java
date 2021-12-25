b0ef266bede85b823529865639c8de61c56af6bc
volRender <litvishko.render@gmail.com>
2021-12-25 14:51:42 +0300

    Project configuration.

---
+++ src/main/java/com/litvishko/spring/security/Test.java
@@ -0,0 +1,4 @@
+package com.litvishko.spring.security;
+
+public class Test {
+}
---
+++ src/main/java/com/litvishko/spring/security/configuration/SecurityUsers.java
@@ -0,0 +1,19 @@
+package com.litvishko.spring.security.configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@EnableWebSecurity
+public class SecurityUsers extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("daniil").password("daniil").roles("EMPLOYEE"))
+                .withUser(userBuilder.username("vlad").password("vlad").roles("HR"))
+                .withUser(userBuilder.username("misha").password("misha").roles("MANAGER", "HR"));
+    }
+}
